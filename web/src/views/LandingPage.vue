4dc9e61e4cab76868d6d91995eeba2e23a20557a
John Lucas <csaionaraa@gmail.com>
2023-06-22 21:03:56 -0300

    start

--- web/src/views/LandingPage.vue
+++ web/src/views/LandingPage.vue
@@ -9,16 +9,37 @@
             </ul>
             <h1 class="menu">&#9776;</h1>
         </nav>
+        <div class="modal" v-if="showModal">
+            <div class="modal-content">
+                <span class="close" @click="closeModal">&times;</span>
+                <h2>Cadastro</h2>
+                <form @submit.prevent="submitForm">
+                    <div class="roww">
+                    <label for="email" class="label">Email:</label>
+                    <input type="email" id="email" name="email" v-model="email" required>
+                </div>
+                <div class="roww">
+                    <label for="password" class="label">Senha:</label>
+                    <input type="password" id="password" name="password" v-model="password" required>
+                </div>
+                    <button type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+
         <div class="main">
-            <h1>Chegou a hora de levar seu petshop para o próximo nível<br> com nosso sistema de gestão moderno e eficiente</h1>
-            <a href="#">Explore mais</a>
+            <h1>Chegou a hora de levar seu petshop para o próximo nível<br> com nosso sistema de gestão moderno e eficiente
+            </h1>
+            <a href="#" @click="openModal">Explore mais</a>
         </div>
         <div class="right-box">
             <img src="../assets/imagem/image-rightbox.png" alt="">
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#04294d" fill-opacity="1" d="M0,32L48,53.3C96,75,192,117,288,144C384,171,480,181,576,186.7C672,192,768,192,864,213.3C960,235,1056,277,1152,282.7C1248,288,1344,256,1392,240L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-        </svg>     
+            <path fill="#04294d" fill-opacity="1"
+                d="M0,32L48,53.3C96,75,192,117,288,144C384,171,480,181,576,186.7C672,192,768,192,864,213.3C960,235,1056,277,1152,282.7C1248,288,1344,256,1392,240L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
     </div>
     <div class="service">
         <h2>Serviços</h2>
@@ -50,18 +71,44 @@
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            showModal: false,
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        openModal() {
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
+        },
+        submitForm() {
+            // Lógica para enviar o formulário (por exemplo, fazer uma requisição ao servidor)
+            console.log('Email:', this.email);
+            console.log('Senha:', this.password);
+            // Limpar campos após o envio do formulário
+            this.email = '';
+            this.password = '';
+            // Fechar o modal
+            this.closeModal();
+        }
+    },
+}
 </script>
 
 <style scoped>
-
-.header{
+.header {
     height: 100vh;
     width: 100%;
     background-color: var(--secondary-color);
     position: relative;
 }
-.header nav{
+
+.header nav {
     width: 90%;
     margin: auto;
     display: flex;
@@ -69,31 +116,37 @@
     align-items: center;
     padding: 0.5rem 0 2rem;
 }
-.header .menu{
+
+.header .menu {
     font-size: 25px;
     display: none;
 }
-.header nav img{
+
+.header nav img {
     width: 120px;
 }
-.header nav ul{
+
+.header nav ul {
     display: flex;
 }
+
 .header nav li {
     list-style: none;
     margin: 0 5px;
 }
-.header nav li a{
+
+.header nav li a {
     text-decoration: none;
     text-transform: uppercase;
     padding: 0.2rem 1rem;
     font-size: 15px;
     font-weight: bold;
-    color: var(--primary-color);
+    color: var(--tertiary-color);
     position: relative;
     z-index: 1;
 }
-.header nav li a::before{
+
+.header nav li a::before {
     content: '';
     width: 0%;
     height: 100%;
@@ -105,23 +158,30 @@
     z-index: -1;
     transition: 0.5s linear;
 }
-.header nav li a:hover:before{
+
+.header nav li a:hover:before {
     width: 100%;
 }
-.header nav li a:hover{
+
+.header nav li a:hover {
     color: var(--tertiary-color);
 }
-.header .main{
+
+.header .main {
     text-align: center;
-    
+
 }
-.header .main h1{
+.main h1{
+    color: white;
+}
+.header .main h1 {
     font-size: 2.5rem;
     margin-top: 90px;
     font-family: 'Montserrat', sans-serif;
     margin-bottom: 2rem;
 }
-.header .main a{
+
+.header .main a {
     text-decoration: none;
     width: 350px;
     font-size: 15px;
@@ -133,10 +193,12 @@
     transition: 0.9s;
     color: #e2ebf4cb;
 }
-.header .main a:hover{
+
+.header .main a:hover {
     background: transparent;
     color: black;
 }
+
 svg {
     position: absolute;
     left: 0;
@@ -144,36 +206,44 @@ svg {
     right: 0;
     z-index: 3;
 }
-.header .right-box{
+
+.header .right-box {
     position: absolute;
     right: 0;
     bottom: 0;
 }
-.header .right-box img{
+
+.header .right-box img {
     width: 100%;
 }
+
 /*SERVICE*/
-.service{
+.service {
     padding-top: 1rem;
     width: 80%;
     margin: auto;
     text-align: center;
 }
-.service h2{
+
+.service h2 {
     font-size: 2.5rem;
     color: var(--tertiary-color);
     font-family: 'Montserrat', sans-serif;
 }
-.service p{
+
+
+.service p {
     color: var(--tertiary-color);
     font-size: 17px;
 }
-.service .row{
+
+.service .row {
     margin-top: 5%;
     display: flex;
     justify-content: space-around;
 }
-.service .servicecol{
+
+.service .servicecol {
     flex-basis: 30%;
     border: 1px solid var(--secondary-color);
     color: var(--tertiary-color);
@@ -181,12 +251,13 @@ svg {
     padding: 1rem 0.5rem;
     position: relative;
 }
-.service .service-offer{
-   display: block;
-   width: 60px;
-   height: 60px;
-   border: 3px solid var(--secondary-color);
-   background-color: var(--primary-color);
+
+.service .service-offer {
+    display: block;
+    width: 60px;
+    height: 60px;
+    border: 3px solid var(--secondary-color);
+    background-color: var(--primary-color);
     border-radius: 50%;
     padding: 10px;
     position: absolute;
@@ -195,11 +266,102 @@ svg {
     transform: translateX(-50px);
 }
 
-.service h4{
+.service h4 {
     font-size: 1.3rem;
 }
-.service .service-offer img{
+
+.service .service-offer img {
     width: 100%;
+}
+
+/*Modal*/
+
+.modal {
+  position: fixed;
+  z-index: 9999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 10% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 300px;
+  max-width: 80%;
+  text-align: center;
+  border-radius: 30px;
+}
+
+.close {
+  color: var(--secondary-color);
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: 1s;
+}
+.modal-content h2{
+    color: var(--secondary-color);
+}
+.close:hover,
+.close:focus {
+  color: var(--primary-color);
+  text-decoration: none;
+  cursor: pointer;
+}
+form {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+form label {
+  margin-bottom: 10px;
+  font-weight: bold;
+}
+.label{
+    display: flex;
+}
+form input[type="email"],
+form input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid var(--secondary-color);
+  border: none;
+  border-radius: 4px;
+  background-color: var(--tertiary-color);
+  transition: background-color 0.3s ease;
 
 }
+
+form input[type="email"]:focus,
+form input[type="password"]:focus {
+  background-color: #4c9aff70;
+  outline: none;
+}
+
+form button[type="submit"] {
+  padding: 10px 20px;
+  background-color: #4c9aff;
+  border: none;
+  color: #fff;
+  background-color: var(--secondary-color);
+  cursor: pointer;
+  border-radius: 4px;
+  transition: background-color 0.5s ease;
+}
+
+form button[type="submit"]:hover {
+  background-color: var(--primary-color);
+  transition: 1s;
+}
+
 </style>
