3a5ac40e3d55cec6bc9a075fd7f00006c8f24cc9
John Lucas <joohnzay@gmail.com>
2023-06-28 20:06:10 -0300

    start

--- web/src/views/LandingPage.vue
+++ web/src/views/LandingPage.vue
@@ -5,9 +5,9 @@
             <img src="../assets/imagem/image_logo.png" alt="logo"> <!--ADICIONAR LOGO-->
             <h1 style="color: white; font-weight: bold; font-size: 36px;">Doo Pets</h1>
             <ul id="navbar">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Galeria</a></li>
+                <li><a href="#um">Home</a></li>
+                <li><a href="#dois">Serviços</a></li>
+                <li><a href="#tres">Galeria</a></li>
             </ul>
             <h1 class="menu">&#9776;</h1>
         </nav>
@@ -39,9 +39,9 @@
             <a href="#" @click="openModal">Faça um teste</a>
         </div>
         <div class="right-box">
-            <img src="../assets/imagem/image-rightbox.png" alt="">
+            <img src="../assets/imagem/image-rightbox.png" alt="" >
         </div>
-        <svg style="margin-bottom: -1;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <svg style="margin-bottom: -1;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" id="dois">
             <path fill="#04294d" fill-opacity="1"
                 d="M0,32L48,53.3C96,75,192,117,288,144C384,171,480,181,576,186.7C672,192,768,192,864,213.3C960,235,1056,277,1152,282.7C1248,288,1344,256,1392,240L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
             </path>
@@ -77,7 +77,7 @@
             </div>
         </div>
         <!--Carousel-->
-        <div class="carouselContainer">
+        <div class="carouselContainer" id="tres">
             <div class="carousel">
                 <h2 style="margin-bottom: 50px;">Galeria de Imagens</h2>
                 <div class="carousel-inner">
